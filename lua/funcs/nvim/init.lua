84cbff807e7a50d2b149bd79b2d8b47f50269426
aca <acadx0@gmail.com>
2020-10-04 17:56:18 +0900

    refactor

---
+++ lua/funcs/nvim/init.lua
@@ -0,0 +1,28 @@
+local M = {}
+local vim = vim
+
+function M.yankpath()
+    local fp = vim.call("expand", "%:p")
+    fp = fp:gsub(vim.call("expand", "~"), "~")
+
+    local curpos = vim.fn.getcurpos()
+
+    print(fp .. ":" .. curpos[2])
+    if fp == "" then
+        return
+    end
+
+    if vim.call("executable", "pbcopy") then
+        vim.call("system", "pbcopy", fp .. ":" .. curpos[2])
+    else
+        error("pbcopy not found")
+    end
+end
+
+function M.remove_file()
+    local f = vim.fn.expand("%")
+    print("rm", f)
+    os.remove(f)
+end
+
+return M
